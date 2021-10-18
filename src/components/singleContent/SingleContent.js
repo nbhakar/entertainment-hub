0af47655669a60a424466cbfbc73f822a883ec0c
Nitin <nbhakar20@gmail.com>
2021-10-18 21:41:59 +0530

    first-commit

--- src/components/singleContent/SingleContent.js
+++ src/components/singleContent/SingleContent.js
@@ -1,6 +1,7 @@
-import { Badge } from '@material-ui/core';
-import {img_300, unavailable} from '../../config/config'
-import './SingleContent.css'
+import { Badge } from "@material-ui/core";
+import { img_300, unavailable } from "../../config/config";
+import "./SingleContent.css";
+import ContentModal from '../ContentModal/ContentModal'
 
 const SingleContent = ({
   id,
@@ -10,14 +11,24 @@ const SingleContent = ({
   media_type,
   vote_average,
 }) => {
-  return <div className="media">
-    <Badge badgeContent={vote_average} color={vote_average>6? "primary": "secondary"}/>
-      <img className="poster" src={poster? `${img_300}/${poster}` : unavailable} alt={title} />
+  return (
+    <ContentModal media_type={media_type} id={id} >
+      <Badge
+        badgeContent={vote_average}
+        color={vote_average > 6 ? "primary" : "secondary"}
+      />
+      <img
+        className="poster"
+        src={poster ? `${img_300}/${poster}` : unavailable}
+        alt={title}
+      />
       <b className="title">{title}</b>
-      <span className="subTitle">{media_type === "tv" ? "TV Series": "Movie"}
-      <span className="subTitle">{date}</span>
+      <span className="subTitle">
+        {media_type === "tv" ? "TV Series" : "Movie"}
+        <span className="subTitle">{date}</span>
       </span>
-  </div>;
+    </ContentModal>
+  );
 };
 
 export default SingleContent;
